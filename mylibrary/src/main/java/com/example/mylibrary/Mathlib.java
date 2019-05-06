aa9a93fc443f1afc3282337c84fec6ec00d18444
hardrock28 <50201375+hardrock28@users.noreply.github.com>
2019-05-06 13:02:03 +0530

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/Mathlib.java
@@ -0,0 +1,30 @@
+package com.example.mylibrary;
+
+public class Mathlib {
+
+    public static  int plus(int a,int b)
+    {
+        return a+b;
+    }
+
+    public static  int minus(int a,int b)
+    {
+        return a-b;
+    }
+
+    public static  int multiply(int a,int b)
+    {
+        return a*b;
+    }
+
+    public static float  divide(int a,int b)
+    {
+        if (b==0)
+            throw  new IllegalArgumentException("Divide by Zero Error");
+        else
+            return a/b;
+    }
+
+
+
+}
